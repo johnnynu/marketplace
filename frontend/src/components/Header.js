2f59dd2065c3a873e00b9c560784cd77b2c51b9a
johnnynu <johnnynu98@gmail.com>
2021-01-07 03:07:24 -0800

    added default items to test

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
 		<header>
 			<Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
 				<Container>
-					<Navbar.Brand href="/">Debbie's Marketplace</Navbar.Brand>
+					<Navbar.Brand href="/">Marketplace</Navbar.Brand>
 					<Navbar.Toggle aria-controls="basic-navbar-nav" />
 					<Navbar.Collapse id="basic-navbar-nav">
 						<Nav className="ml-auto">
