f59acec60a5d91f585691191f464f5547f8289a1
johnnynu <johnnynu98@gmail.com>
2021-02-12 20:10:49 -0800

    worked on user signin and registration in backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,14 @@
 import express from "express";
-import { authUser } from "../controllers/userController.js";
+import {
+	authUser,
+	registerUser,
+	getUserProfile,
+} from "../controllers/userController.js";
 const router = express.Router();
+import { protect } from "../middleware/authMiddleware.js";
 
 router.post("/login", authUser);
+router.route("/").post(registerUser);
+router.route("/profile").get(protect, getUserProfile);
 
 export default router;
