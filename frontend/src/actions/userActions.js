4256c507d70651c16128fdd098a874e3ff37912e
johnnynu <johnnynu98@gmail.com>
2021-03-11 18:45:39 -0800

    fixed bug showing account details

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -16,6 +16,7 @@ import {
 	USER_UPDATE_PROFILE_REQUEST,
 	USER_UPDATE_PROFILE_FAIL,
 } from "../constants/userConstants";
+import { ORDER_USER_LIST_RESET } from "../constants/orderConstants";
 
 export const login = (email, password) => async (dispatch) => {
 	try {
@@ -55,6 +56,8 @@ export const login = (email, password) => async (dispatch) => {
 export const logout = () => (dispatch) => {
 	localStorage.removeItem("userInfo");
 	dispatch({ type: USER_LOGOUT });
+	dispatch({ type: USER_DETAILS_RESET });
+	dispatch({ type: ORDER_USER_LIST_RESET });
 };
 
 export const register = (name, email, password) => async (dispatch) => {
